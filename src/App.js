9c2dd54d099143492993723ca4c69be89e19024f
Kanu Charan Nahak <kanu.nahak.79@gmail.com>
2023-08-19 18:52:25 +0530

    new commit for virtual keyboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Keyboard from "react-simple-keyboard";
 
-function App() {
+// Instead of the default import, you can also use this:
+// import { KeyboardReact as Keyboard } from "react-simple-keyboard";
+
+import "react-simple-keyboard/build/css/index.css";
+import "./App.css";
+
+export default function App() {
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const keyboard = useRef();
+
+  const onChange = input => {
+    setInput(input);
+    console.log("Input changed", input);
+  };
+
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
+
+  const onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
+
+  const onChangeInput = event => {
+    const input = event.target.value;
+    setInput(input);
+    keyboard.current.setInput(input);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={input}
+        placeholder={"Tap on the virtual keyboard to start"}
+        onChange={onChangeInput}
+      />
+      <Keyboard
+        keyboardRef={r => (keyboard.current = r)}
+        layout={{
+          default: ['1 2 3', '4 5 6', '7 8 9', '0 {bksp}'],
+        }}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+          
+        />
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NumKey from './Numkey';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <NumKey/> */}
   </React.StrictMode>
 );
 
